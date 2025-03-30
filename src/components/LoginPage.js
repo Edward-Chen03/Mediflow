d0a70c08ee69039a8322d53a30bdb6a25be4134b
Edward Chen <kulathatguy@gmail.com>
2025-03-29 21:55:51 -0400

    Small Update

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -5,8 +5,8 @@ import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
 
 export default function LoginPage() {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
+  const [username, setUsername] = useState('testadmin@gmail.com');
+  const [password, setPassword] = useState('testpassword');
   const navigate = useNavigate();
 
   const handleForgotPasswordClick = () => {
